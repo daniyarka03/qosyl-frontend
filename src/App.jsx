82e215091783dfb2d4f4827b04d4e33ee705251e
Daniyar Orazov <orazov.video@gmail.com>
2023-08-09 11:01:52 +0600

    Fixed bugs in App.jsx with uncorrect link to files based on new structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import styles from "./App.module.sass";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignIn from "../src/pages/SignIn";
-import SignUp from "../src/pages/SignUp";
-import Button from "./components/Button/Button";
-const App = () => {
+import './App.module.sass'
+
+function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<SignIn/>}/>
-        <Route path='/registration' element={<SignUp/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+    </div>
   )
 }
 
