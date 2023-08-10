613496b1e824de00721faeacf563c70ced77bf48
DayTroy <shadowyguy5@gmail.com>
2023-08-10 12:05:20 +0300

    Made responsive sign in and up pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "./SignInUp.module.sass";
+import { Link } from "react-router-dom";
+import projectLogo from "../assets/project-logo.svg";
+import Input from "../components/Input/Input";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <img className={styles.header__logo} src={projectLogo} />
+        <h2 className={styles.header__title}>Авторизация</h2>
+      </div>
+      <form action="" className={styles.form}>
+        <Input placeholder="Имя" type="text"/>
+        <Input placeholder="Пароль" type="password"/>
+        <button className={styles.form__button} type="submit">
+          Войти
+        </button>
+        <Link className={styles.form__link} to="/registration">
+          Регистрация
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import styles from "./SignInUp.module.sass";
+import { Link } from "react-router-dom";
+import projectLogo from "../assets/project-logo.svg";
+import Input from "../components/Input/Input";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <img className={styles.header__logo} src={projectLogo} />
+        <h2 className={styles.header__title}>Регистрация</h2>
+      </div>
+      <form action="" className={styles.form}>
+        <Input placeholder="Имя" type="text"/>
+        <Input placeholder="Фамилия" type="text"/>
+        <Input placeholder="Пароль" type="password"/>
+        <Input placeholder="Подтвердить пароль" type="password"/>
+        <button className={styles.form__button} type="submit">
+          Регистрация
+        </button>
+        <Link className={styles.form__link} to="/">
+          Войти в аккаунт
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default SignUp;
