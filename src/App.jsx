4b7e0b71bf9e62c78adcbfb08f68bbb9c75f5d5b
Daniyar Orazov <orazov.video@gmail.com>
2023-08-17 17:17:59 +0600

    Fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import ProjectInfo from './pages/ProjectInfo/ProjectInfo'
 import CreateProject from './pages/CreateProject/CreateProject'
 import ResetPassword from "./pages/Auth/ResetPassword.jsx";
 import ResetPasswordConfirm from "./pages/Auth/ResetPasswordConfirm.jsx";
-import {connect, Provider} from "react-redux";
+import {Provider} from "react-redux";
 import store from "./store.js";
 import Layout from "./hocs/Layout.jsx";
 import Profile from './pages/Profile/Profile'
 import Users from './pages/Users/Users'
 import Notifications from './pages/Notifications/Notifications'
-const App = ({isAuthenticated}) => {
+const App = () => {
   return (
     <div className={styles.App}>
       <Provider store={store}>
@@ -22,11 +22,11 @@ const App = ({isAuthenticated}) => {
             <Routes>
               <Route path='/' element={<SignIn/>}/>
               <Route path='/registration' element={<SignUp/>}/>
-                <Route path="/projects" element={isAuthenticated ? <Projects /> : <SignIn />} />
+                <Route path="/projects" element={<Projects />} />
               <Route path='/reset-password' element={<ResetPassword/>}/>
               <Route path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm/>}/>
-              <Route path='/project-info' element={isAuthenticated ? <ProjectInfo/> : <SignIn />}/>
-              <Route path='/create-project' element={isAuthenticated ? <CreateProject/> : <SignIn />}/>
+              <Route path='/project-info' element={<ProjectInfo/>}/>
+              <Route path='/create-project' element={<CreateProject/>}/>
               <Route path='/profile' element= {<Profile/>} />
               <Route path='/users' element= {<Users/>} />
               <Route path='/notifications' element= {<Notifications/>} />
