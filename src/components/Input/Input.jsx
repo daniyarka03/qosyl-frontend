6c695e978fe95dcbdd236eb3e1aa67d010a5a91a
DayTroy <shadowyguy5@gmail.com>
2023-08-19 21:25:29 +0300

    Implementedd pushing project to server(half)

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import styles from "../Input/Input.module.sass";
 
-
-const Input = ({ type, placeholder, withIcon = false, imageSrc, required, value, onChange, minLength }) => {
-
-
+const Input = ({
+  type,
+  placeholder,
+  withIcon = false,
+  imageSrc,
+  required,
+  value,
+  onChange,
+  minLength,
+  handleChange,
+}) => {
   return withIcon ? (
     <div className={styles.input__wrapper}>
       <input
         className={styles.input__right}
         placeholder={placeholder}
         type={type}
-
         value={value}
-        onChange={onChange}
+        onChange={handleChange}
         required={required}
         minLength={minLength}
-
-
       />
       <img className={styles.input__img} src={imageSrc} />
     </div>
   ) : (
     <div className={styles.input__wrapper}>
-
-      <input className={styles.input} placeholder={placeholder} type={type}  value={value}
-             onChange={onChange}
-             required={required} />
-
+      <input
+        className={styles.input}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={handleChange}
+        required={required}
+      />
     </div>
   );
 };
--- src/pages/CreateProject/CreateProject.jsx
+++ src/pages/CreateProject/CreateProject.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CreateProject.module.sass";
 import projectLogo from "../../assets/project-logo.svg";
 import Input from "../../components/Input/Input";
 import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
+
+const src = "http://127.0.0.1:8000/api/projects/create/";
 
 const CreateProject = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleInputChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    axios
+      .post(src, {
+        title: title,
+        description: description,
+        type: "Social app",
+        image_src: "1212312312321",
+        contact: "f0k u",
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <Navbar />
@@ -17,14 +44,20 @@ const CreateProject = () => {
           />
           <h2 className={styles.header__title}>Создание проекта</h2>
         </div>
-        <form action="" className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <Input
             placeholder="Название проекта"
             type="text"
             name="text"
             id="projectName"
+            handleChange={handleInputChange}
           />
-          <textarea className={styles.textarea} placeholder="Описание" />
+          <textarea
+            className={styles.textarea}
+            placeholder="Описание"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
           <button className={styles.form__button} type="submit">
             Создать
           </button>
