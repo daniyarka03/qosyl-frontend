48a07de53913bf98462e11ada5a4bffd301a1c0f
DayTroy <shadowyguy5@gmail.com>
2023-08-22 17:50:41 +0300

    Added switch like icons when clicking

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PostCard.module.sass";
 import postCardAvatar from "../../assets/post-card-avatar.png";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import likeIcon from "../../assets/like-icon.svg";
+import likeIconUnfilled from "../../assets/like-outline-icon.svg";
+import likeIconFilled from "../../assets/like-icon.svg";
 import commentIcon from "../../assets/comment-icon.svg";
+
 const PostCard = ({ isUserPost, authorName, postID, content }) => {
+  const [isLiked, setIsLiked] = useState(false);
   const navigate = useNavigate();
-  const deletePostURL = `http://127.0.0.1:8000/api/posts/${postID}/delete/`
+  const deletePostURL = `http://127.0.0.1:8000/api/posts/${postID}/delete/`;
   const deletePost = () => {
     axios
       .delete(deletePostURL)
@@ -29,24 +32,39 @@ const PostCard = ({ isUserPost, authorName, postID, content }) => {
         <p className={styles.post__description}>{content}</p>
         <div className={styles.post__actions}>
           <div className={styles.post__action}>
-            <img className={styles.post__action__img} src={likeIcon} alt="like-icon" />
+            <img
+              className={styles.post__action__img}
+              src={isLiked ? likeIconFilled : likeIconUnfilled}
+              alt="like-icon"
+              onClick={() => setIsLiked(!isLiked)}
+            />
             <p className={styles.post__action__count}>94</p>
           </div>
           <div className={styles.post__action}>
-            <img className={styles.post__action__img} src={commentIcon} alt="comment-icon" />
+            <img
+              className={styles.post__action__img}
+              src={commentIcon}
+              alt="comment-icon"
+            />
             <p className={styles.post__action__count}>2</p>
           </div>
         </div>
-        {isUserPost && (<div className={styles.post__buttons}>
-          <button
-          className={`${styles.post__button} ${styles.post__edit}`}
-          onClick={() => navigate(`/edit-post/${postID}`)}
-        >
-          Изменить
-        </button>
-        <button className={`${styles.post__button} ${styles.post__delete}`} onClick={deletePost}>
-          Удалить
-        </button></div>)}
+        {isUserPost && (
+          <div className={styles.post__buttons}>
+            <button
+              className={`${styles.post__button} ${styles.post__edit}`}
+              onClick={() => navigate(`/edit-post/${postID}`)}
+            >
+              Изменить
+            </button>
+            <button
+              className={`${styles.post__button} ${styles.post__delete}`}
+              onClick={deletePost}
+            >
+              Удалить
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
