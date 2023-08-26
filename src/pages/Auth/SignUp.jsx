42a7834895da1295484d49d25763e431a2ebf5b0
Daniyar Orazov <orazov.video@gmail.com>
2023-08-26 19:22:33 +0600

    Fix bugs

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -28,7 +28,7 @@ const handleSubmit = async (e) => {
 
     console.log(username)
 
-    const response = await fetch('http://127.0.0.1:8000/auth/users/', {
+    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/auth/users/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
