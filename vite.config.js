b463ca6a767b81a01c6a93f3faa128dba7d3332e
root <root@server.gohost.kz>
2023-08-26 20:38:50 +0000

    edited for server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import fs from 'fs';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://109.248.170.181:8000'
+    },
+    https: {
+      key: fs.readFileSync('/key.pem'),
+      cert: fs.readFileSync('/cert.pem'),
+    },
+  },
   plugins: [react()],
 })
