2e71d9c4db06cf39288806217a16364d71803d96
Daniyar Orazov <orazov.video@gmail.com>
2023-09-15 21:00:15 +0600

    Added register additional rule

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -35,9 +35,14 @@ const SignUp = ({location, history}) => {
 const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!name || !email || !password || !confirmPassword) {
-        setMessage('Заполните все поля');
-        console.log(name, email, password, confirmPassword)
+    if (!name || !email || !password || !confirmPassword || !document.getElementById('myCheckbox').checked) {
+        if (!document.getElementById('myCheckbox').checked) {
+            setMessage('Вы не подтвердили что вам 16+ лет');
+        } else {
+            setMessage('Заполните все поля');
+            console.log(name, email, password, confirmPassword)
+        }
+
     } else {
         if (password !== confirmPassword) {
             setMessage('Пароли не совпадают');
@@ -58,11 +63,15 @@ const handleSubmit = async (e) => {
       </div>
         {message && <div className={styles.error_message}>{message}</div>}
       <form action="#" className={styles.form} onSubmit={handleSubmit}>
-        <Input placeholder="Имя" type="text" id="signUpFormFirstName" name="name" value={name} onChange={(e) => setName(e.target.value)} class={styles.input__register}  />
-        <Input placeholder="Email" type="text" id="signUpFormEmail" name="email" value={email} onChange={(e) => setEmail(e.target.value)} class={styles.input__register} />
-        <Input placeholder="Пароль" type="password" id="signUpFormPassword" name="password" value={password} onChange={(e) => setPassword(e.target.value)} class={styles.input__register} />
-        <Input placeholder="Подтвердить пароль" type="password" id="signUpFormCofirmPassword" name="confirm_password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} class={styles.input__register}/>
+        <Input placeholder="Имя" type="text" id="signUpFormFirstName" name="name" value={name} onChange={(e) => setName(e.target.value)} style={{marginTop: "3rem"}}  />
+        <Input placeholder="Email" type="email" id="signUpFormEmail" name="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{marginTop: "3rem"}} />
+        <Input placeholder="Пароль" type="password" id="signUpFormPassword" name="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{marginTop: "3rem"}} />
+        <Input placeholder="Подтвердить пароль" type="password" id="signUpFormCofirmPassword" name="confirm_password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} style={{marginTop: "3rem"}} />
+         <div className={styles.checkbox}>
+             <input type="checkbox" id="myCheckbox" name="myCheckbox" value="checkboxValue" />
 
+             <label className={styles.checkbox__label} htmlFor="myCheckbox">Вы подтверждаете что вам 16+ лет</label>
+         </div>
         <button className={styles.form__button} type="submit">
           Регистрация
         </button>
